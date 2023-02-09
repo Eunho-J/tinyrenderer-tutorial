09058b5b12686734c21c6d3ffb95fced1aa202cc
Eunho-J <jho6394@gmail.com>
2023-02-09 13:39:12 +0900

    merge

---
+++ lab03/main.cpp
@@ -0,0 +1,85 @@
+#include <vector> 
+#include <iostream> 
+#include <cstdlib>
+#include <limits>
+#include "geometry.h"
+#include "tgaimage.h" 
+#include "model.h"
+ 
+const int width  = 1200; 
+const int height = 1200; 
+ 
+Vec3f barycentric(Vec2i *pts, Vec2i P) { 
+    Vec3f u = Vec3f(pts[2].x-pts[0].x, pts[1].x-pts[0].x, pts[0].x-P.x)^Vec3f(pts[2].y-pts[0].y, pts[1].y-pts[0].y, pts[0].y-P.y);
+    /* 마지막 인자가 0이면 해가 없다. 그런데 여기서는 어짜피 정수 좌표만을 사용하므로, 
+       절대값이 1보다 작은 것이 0임을 의미한다. */
+    if (std::abs(u.z)<1) return Vec3f(-1,1,1);
+    return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z); 
+} 
+ 
+void triangle(Vec2i *pts, float* zbuffer, TGAImage &image, TGAColor color) { 
+    Vec2i bboxmin(image.get_width()-1,  image.get_height()-1); 
+    Vec2i bboxmax(0, 0); 
+    Vec2i clamp(image.get_width()-1, image.get_height()-1); 
+    /* 이미지 전체의 점에 대해서 각 삼각형에 대해 검사를 하면 너무 많은 연산을 해야한다. 
+       따라서 삼각형을 둘러싼 최소 크기의 Bounding Box를 구하여,
+       해당 Box 안의 점에 대해서만 연산을 수행한다. */
+    for (int i=0; i<3; i++) 
+    { 
+        bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
+        bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
+
+        bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+        bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+    } 
+    Vec3i P; 
+    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) 
+    { 
+        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) 
+        { 
+            Vec3f bc_screen  = barycentric(pts, P); 
+            //i, j, k중 하나라도 음수이면 삼각형 밖의 점이다.
+            if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue;
+            P.z = 0;
+            image.set(P.x, P.y, color); 
+        } 
+    } 
+} 
+
+
+int main(int argc, char** argv) { 
+    Model* model;
+    float* zbuffer = new float[width*height];
+    for (int i = 0; i < width*height; i++) zbuffer[i] = -std::numeric_limits<float>::max();
+
+    if (2==argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("obj/african_head.obj");
+    }
+
+    TGAImage image(width, height, TGAImage::RGB);
+    Vec3f light_dir(0,0,-1);
+    for (int i=0; i<model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
+        for (int j=0; j<3; j++) {
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.);
+            world_coords[j]  = v;
+        }
+        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
+        n.normalize();
+        float intensity = n*light_dir;
+        Vec2i pts[3] = {screen_coords[0], screen_coords[1], screen_coords[2]};
+        if (intensity>0) {
+            triangle(pts, zbuffer, image, TGAColor(intensity*255, intensity*255, intensity*255, 255));
+        }
+    }
+
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
+    delete model;
+    return 0;
+}
